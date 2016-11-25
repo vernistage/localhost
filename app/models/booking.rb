602bee27d4ef26170ddfffdd8fffdf8c47f00924
vernistage <sdickers10@gmail.com>
2016-11-24 19:08:24 -0600

    validations for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
 	has_one :review
 	belongs_to :guest, class_name: "User", foreign_key: :guest_id
 	belongs_to :host, class_name: "User", foreign_key: :host_id
+
+	validates: :guest_id, :host_id, :meet_location, :summary, presence: true
 end
