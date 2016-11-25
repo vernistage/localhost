6e34c1a72c8a09a32b08f4a89f4ae67d45338cfb
Sarah Dickerson <email": null>
2016-11-25 12:31:56 -0600

    finish user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,147 +30,55 @@ describe User do
       expect(host.encrypted_password).not_to eq ""
     end
 
+    it "host has a homebase" do
+      expect(host.homebase).to eq "hosthome"
+    end
+
+    it "host has a homebase" do
+      expect(host.host).to be true
+    end
   end
 
-  # describe "#throw_for_computer" do
-  #   context "when the game already has a computer throw" do
-  #     it "returns the computer throw" do
-  #       game.computer_throw = "paper"
-  #       expect(game.throw_for_computer).to eq "paper"
-  #     end
-  #   end
+  describe "guest attributes" do
+    it "is not a host" do
+      expect(guest.host).to be false
+    end
+  end
 
-  #   context "when the game does not have a computer throw" do
-  #     it "generates a throw" do
-  #       expect(game.throw_for_computer).to be_in Game::THROWS
-  #     end
+  describe "guest associations" do
+    it "guest has bookings" do
+        expect(guest.guest_bookings).to be_a(Booking::ActiveRecord_Associations_CollectionProxy)
+    end
 
-  #     it "sets the computer throw to the generated throw" do
-  #       expect(game.computer_throw).to be_nil
-  #       generated_computer_throw = game.throw_for_computer
-  #       expect(game.computer_throw).to eq generated_computer_throw
-  #     end
-  #   end
-  # end
+    it "guest has reviews" do
+        expect(guest.guest_reviews).to be_a(Review::ActiveRecord_Associations_CollectionProxy)
+    end
 
-  # describe "#computer_throw" do
-  #   describe "validations" do
-  #     it "is valid when it's rock" do
-  #       game.computer_throw = 'rock'
-  #       game.valid?
-  #       expect(game.errors[:computer_throw]).to be_empty
-  #     end
+    it "guest does not have categories" do
+        expect(guest.categories).to be_empty
+    end
 
-  #     it "is valid when it's paper" do
-  #       game.computer_throw = 'paper'
-  #       game.valid?
-  #       expect(game.errors[:computer_throw]).to be_empty
-  #     end
+    it "guest does not have tags" do
+        expect(guest.tags).to be_empty
+    end
+  end
 
-  #     it "is valid when it's scissors" do
-  #       game.computer_throw = 'scissors'
-  #       game.valid?
-  #       expect(game.errors[:computer_throw]).to be_empty
-  #     end
+  describe "host associations" do
+    it "host has bookings" do
+        expect(host.host_bookings).to be_a(Booking::ActiveRecord_Associations_CollectionProxy)
+    end
 
-  #     it "is NOT valid when it's pineapple" do
-  #       game.computer_throw = 'pineapple'
-  #       game.valid?
-  #       expect(game.errors[:computer_throw]).to_not be_empty
-  #     end
-  #   end
-  # end
+    it "host has reviews" do
+        expect(host.host_reviews).to be_a(Review::ActiveRecord_Associations_CollectionProxy)
+    end
 
-  # describe "#user_throw" do
-  #   describe "validations" do
-  #     it "is valid when it's rock" do
-  #       game.user_throw = 'rock'
-  #       game.valid?
-  #       expect(game.errors[:user_throw]).to be_empty
-  #     end
+    it "host has categories" do
+      expect(host.categories).to be_a(Category::ActiveRecord_Associations_CollectionProxy)
+    end
 
-  #     it "is valid when it's paper" do
-  #       game.user_throw = 'paper'
-  #       game.valid?
-  #       expect(game.errors[:user_throw]).to be_empty
-  #     end
+    it "host has tags" do
+      expect(host.tags).to be_a(Tag::ActiveRecord_Associations_CollectionProxy)
+    end
+  end
 
-  #     it "is valid when it's scissors" do
-  #       game.user_throw = 'scissors'
-  #       game.valid?
-  #       expect(game.errors[:user_throw]).to be_empty
-  #     end
-
-  #     it "is NOT valid when it's pineapple" do
-  #       game.user_throw = 'pineapple'
-  #       game.valid?
-  #       expect(game.errors[:user_throw]).to_not be_empty
-  #     end
-  #   end
-  # end
-
-  # describe "#winner" do
-  #   it "computer is the winner when the computer throw beats the user throw" do
-  #     game.user_throw = "scissors"
-  #     game.computer_throw = "rock"
-
-  #     expect(game.winner).to eq "computer"
-  #   end
-
-  #   it "user is the winner when the user throw beats the computer throw" do
-  #     game.user_throw = "rock"
-  #     game.computer_throw = "scissors"
-
-  #     expect(game.winner).to eq "user"
-  #   end
-  # end
-
-  # describe "#winner_throw" do
-  #   it "returns the user throw when the user wins" do
-  #     game.user_throw = "paper"
-  #     game.computer_throw = "rock"
-
-  #     expect(game.winner_throw).to eq game.user_throw
-  #   end
-
-  #   it "returns the computer throw when the computer wins" do
-  #     game.user_throw = "rock"
-  #     game.computer_throw = "paper"
-
-  #     expect(game.winner_throw).to eq game.computer_throw
-  #   end
-  # end
-
-  # describe "#loser_throw" do
-  #   it "returns the user throw when the computer wins" do
-  #     game.user_throw = "paper"
-  #     game.computer_throw = "rock"
-
-  #     expect(game.winner_throw).to eq game.user_throw
-  #   end
-
-
-  #   it "returns the computer throw when the user wins" do
-  #     game.user_throw = "paper"
-  #     game.computer_throw = "rock"
-
-  #     expect(game.winner_throw).to eq game.user_throw
-  #   end
-  # end
-
-  # describe "#user_won?" do
-  #   it "returns true if user won" do
-  #     game.user_throw = 'paper'
-  #     game.computer_throw = 'rock'
-
-  #     expect(game.user_won?).to be true
-  #   end
-
-  #   it "returns false if user lost" do
-  #     game.user_throw = 'rock'
-  #     game.computer_throw = 'paper'
-
-  #     expect(game.user_won?).to be false
-  #   end
-  # end
 end
