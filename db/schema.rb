ad2bee4bace8f0a924ceeafecca28e8a16a3cfb6
Luke Westerfield <email": null>
2016-11-25 17:54:52 -0600

    fuck this shit

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20161123231131) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
