04356424425d46d529c28f878b899554538ae583
vernistage <sdickers10@gmail.com>
2016-11-24 19:08:50 -0600

    validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
 	belongs_to :booking
 	delegate :guest, :to => :booking, :allow_nil => true
 	delegate :host,  :to => :booking, :allow_nil => true
+
+	validates: :booking_id, presence: :true, uniqueness: true
+	validates: :body, :rating, presence: :true
 end
