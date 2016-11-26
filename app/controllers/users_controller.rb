4e70cc4f6e346a0cd41a4ff3af9e5cd45422622f
vernistage <sdickers10@gmail.com>
2016-11-26 09:29:56 -0600

    refactor error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @category = Category.find(params[:category])
     @location = session[:location]
     @users = User.where(homebase: @location)
+    @error = "We're sorry, no hosts yet exist in #{@location} for #{@category}."
   end
 
 end
