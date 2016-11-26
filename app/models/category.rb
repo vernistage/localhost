da5aaa88d14dfaa5ea09f851519ee923d402a7e9
Luke Westerfield <email": null>
2016-11-25 18:28:45 -0600

    what the fuck

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,8 @@ class Category < ApplicationRecord
 	has_many :tags
 	has_many :user_categories
 	has_many :hosts, through: :user_categories
-	
-<<<<<<< HEAD
-	validates :title, :description, uniqueness: true, presence: true
-=======
+
 	validates :title, :description, presence: true
 	validates :title, :description, uniqueness: true
->>>>>>> master
+
 end
