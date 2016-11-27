929ba7a289018f83d725af807bc528196ffe6dfd
vernistage <sdickers10@gmail.com>
2016-11-27 08:51:12 -0600

    initial form structure with styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,11 @@
-class CategoriesController < ApplicationController
+class ReviewsController < ApplicationController
 	
 	def new
+		@review = Review.new
 	end
 
 	def create
+		binding.pry
 	end
+
 end
