8bc39c64ec85ce6e14bf0ae2e02294f3c525ce59
Sarah Dickerson <email": null>
2016-11-27 23:56:52 -0600

    comment out jquery code that was throwin bugs

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -3,17 +3,17 @@ $('body').css({
   'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'
 });
 
-$(document).ready(function(){
-   $('body').on('click', '#search', function(event){
-    event.preventDefault();
-    var t = $(this)
-    $.ajax({
-      type: 'GET',
-      url: "/categories"
-    }).done(function(response){
-      t.parent().parent().animate({'marginTop' : "-=150px" });
-    })
-  })
-})
+// $(document).ready(function(){
+//    $('body').on('click', '#search', function(event){
+//     event.preventDefault();
+//     var t = $(this)
+//     $.ajax({
+//       type: 'GET',
+//       url: "/categories"
+//     }).done(function(response){
+//       t.parent().parent().animate({'marginTop' : "-=150px" });
+//     })
+//   })
+// })
 
 
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,9 +3,6 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
     session[:location] = params[:location]
-    respond_to do |format|
-      format.html {render layout: false}
-    end
   end
 
   def show
