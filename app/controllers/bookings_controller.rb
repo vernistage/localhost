f2db4c2ca9bda2249a8bcebd9113aeb227c4b919
Luke Westerfield <email": null>
2016-11-29 20:12:43 -0600

    timezones working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,40 +7,37 @@ class BookingsController < ApplicationController
   def post_booking
 
     times = {"Austin" =>  21600, "Chicago" =>  21600, "New York" => 18000, "Los Angeles" => 28800, "Seattle" =>  28800}
+    binding.pry
+    start_time = params[:start_time].to_datetime
+    start_time = start_time.utc + times[session[:location]]
 
-    start_time = params[:start_time]
-    start_time += ":00+00:00"
-    start_time = DateTime.strptime(start_time).utc + times[session[:location]]
-    end_time = params[:end_time]
-    end_time += ":00+00:00"
-    end_time = DateTime.strptime(end_time).utc + times[session[:location]]
+    end_time = params[:end_time].to_datetime
+    end_time = end_time.utc + times[session[:location]]
+
     Booking.create(guest_id: User.find_by(email: params[:guest]).id, host_id: current_user.id, meet_location: params[:meet_location], start_time: start_time, end_time: end_time, summary: params[:summary])
-    redirect_to users_show_path(@current_user)
+    redirect_to users_show_path(current_user)
   end
 
   def edit_booking
+    times = {"Austin" =>  21600, "Chicago" =>  21600, "New York" => 18000, "Los Angeles" => 28800, "Seattle" =>  28800}
     @booking = Booking.find(params[:id])
+    @booking.start_time -= times[current_user.homebase]
+    @booking.end_time -= times[current_user.homebase]
   end
 
   def update_booking
    times = {"Austin" =>  21600, "Chicago" =>  21600, "New York" => 18000, "Los Angeles" => 28800, "Seattle" =>  28800}
 
-    start_time = params[:start_time]
-    start_time += ":00+00:00"
-    start_time = DateTime.strptime(start_time).utc + times[session[:location]]
-    end_time = params[:end_time]
-    end_time += ":00+00:00"
-    end_time = DateTime.strptime(end_time).utc + times[session[:location]]
+    start_time = params[:booking][:start_time].to_datetime
+    start_time = start_time.utc + times[session[:location]]
 
-    b = Booking.find(params[:booking_id])
-    b.guest_id = User.find_by(email: params[:guest]).id
-    b.host_id = current_user.id
-    b.meet_location = params[:meet_location]
-    b.start_time = params[:start_time]
-    b.end_time = params[:end_time]
-    b.summary = params[:summary]
+    end_time = params[:booking][:end_time].to_datetime
+    end_time = end_time.utc + times[session[:location]]
+
+    b = Booking.find(params[:booking][:id])
+    b.update(params.require(:booking).permit(:id, :guest_id, :host_id, :meet_location, :start_time, :end_time, :summary))
     b.save
-    redirect_to users_show_path(@current_user)
+    redirect_to users_show_path(current_user)
   end
 
   def cancel
