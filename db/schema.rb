7f7f0f2e7182828fecf2cb52d787201a3524858b
Sarah Dickerson <email": null>
2016-11-30 17:21:25 -0600

    add geocoder gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127182403) do
+ActiveRecord::Schema.define(version: 20161130231911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20161127182403) do
     t.text     "summary"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "categories", force: :cascade do |t|
