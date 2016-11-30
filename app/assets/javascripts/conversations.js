ba16beceee28266e3590b2bc570b442b19b07f97
Matt McGee <email": null>
2016-11-30 01:41:06 -0600

    ajax message send. button won't click twice. need to ajax polling

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
 
-  // $('#send-message-form').on('submit', function(e){
-  //   e.preventDefault();
-  //   var url = this.action;
-  //   var data = $(this).serialize();
-  //   var form = this;
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: url,
-  //     data: data
-  //   }).done(function(response){
-  //     debugger;
-  //   });
-  // });
+  $('body').on('submit', '#send-message-form', function(e){
+    e.preventDefault();
+    var url = this.action;
+    var data = $(this).serialize();
+    var form = $(this);
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: data
+      }).done(function(response){
+        $("#chat-messages").children().last().after(response);
+    });
+    $(this).trigger('reset');
+  });
+  (function(){
+    setInterval(function() {
 
+      }, 4000);
+  })();
 });
+
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,13 @@ class MessagesController < ApplicationController
 
   def create
     @conversation = Conversation.find(params[:id])
-    @conversation.messages.build(user_id: current_user.id, body: params[:message][:body]).save
-    redirect_to conversations_show_path(@conversation)
+    @message = @conversation.messages.build(user_id: current_user.id, body: params[:message][:body])
+    @message.save
+    if request.xhr?
+      render :partial => '/messages/host_show', :locals => {message: @message}
+    else
+      redirect_to conversations_show_path(@conversation)
+    end
   end
 
 end
