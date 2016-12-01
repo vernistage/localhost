003e44a5190f610c232384dab105c2a36a2df8fa
Sarah Dickerson <email": null>
2016-12-01 09:40:53 -0600

    add address to map marker

---
+++ app/assets/javascripts/channels/rating.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+
+//   if (document.getElementsByClassName('star-rating') != null){
+//     $('.star-rating').raty({
+//         path: '/assets/',
+//         readOnly: true,
+//         score: function() {
+//           return $(this).attr('data-score')
+//       }
+//     });
+//   };
+
+//   if (document.getElementById('star-rating') != null){
+//     $('#star-rating').raty({
+//       path: '/assets/',
+//       scoreName: 'review[rating]'
+//     });
+//   };
+
+
+// });
