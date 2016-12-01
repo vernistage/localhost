e6a3209f9f973df01786fd0fc141838d6090efa9
Sarah Dickerson <email": null>
2016-12-01 17:54:02 -0600

    start reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # HOSTS ===============================================================================================
+
+# Start Austin
 addison = User.create!(email: "addison@addison.com", password: "password", name: "Addison", homebase: "Austin", host: true, bio: "nonprofessorial counterestablishment obligingness Querendy planeta uneditable antiwaste interoceanic uncomprehending fogramity unrotating quartet scutellae loveman zambo leaguer witmonger sulpholeic unexplicableness wan balneological achromatophile sheepfacedly hirable")
 img = File.open(File.join(Rails.root, '/public/images/seed/img1.jpg'))
 addison.update(avatar: img)
@@ -31,6 +33,7 @@ reed = User.create!(email: "reed@reed.com", password: "password", name: "Reed", 
 img = File.open(File.join(Rails.root, '/public/images/seed/img8.jpg'))
 reed.update(avatar: img)
 
+# Start Seattle
 morgan = User.create!(email: "morgan@morgan.com", password: "password", name: "Morgan", homebase: "Seattle", host: true, bio: "Roland moralist typometry dominion raught Scythian orientalist Uranian answerer nigrosine milarite bargain plasmodesmic hesitatively Tayassuidae setterwort playfield epigastriocele farraginous waying seck Hymenocallis longspur taxiplane")
 img = File.open(File.join(Rails.root, '/public/images/seed/img9.jpg'))
 morgan.update(avatar: img)
@@ -199,9 +202,9 @@ Category.create!(title: "History", description: "Pay homage to the past", image:
     UserTag.create!(host_id: 6, tag_id: 27)
     UserTag.create!(host_id: 6, tag_id: 28)
   #Host 7
-    UserCategory.create!(host_id: 6, category_id: 4) #user_category_id = 13
-    UserCategory.create!(host_id: 6, category_id: 5)
-    UserCategory.create!(host_id: 6, category_id: 6) #user_category_id =14
+    UserCategory.create!(host_id: 7, category_id: 4) #user_category_id = 13
+    UserCategory.create!(host_id: 7, category_id: 5)
+    UserCategory.create!(host_id: 7, category_id: 6) #user_category_id =14
     Tag.create!(specialty: "LGBTQ niches", category_id: 4)
     Tag.create!(specialty: "Queer clubs", category_id: 4)
     Tag.create!(specialty: "Craft fairs", category_id: 5)
@@ -215,10 +218,10 @@ Category.create!(title: "History", description: "Pay homage to the past", image:
     UserTag.create!(host_id: 7, tag_id: 33)
     UserTag.create!(host_id: 7, tag_id: 34)
   #Host 8
-    UserCategory.create!(host_id: 6, category_id: 7) #user_category_id = 13
-    UserCategory.create!(host_id: 6, category_id: 8)
-    UserCategory.create!(host_id: 6, category_id: 9) #user_category_id =14
-    Tag.create!(specialty: "Austin City Limits festival", c8tegory_id: 7)
+    UserCategory.create!(host_id: 8, category_id: 7) #user_category_id = 13
+    UserCategory.create!(host_id: 8, category_id: 8)
+    UserCategory.create!(host_id: 8, category_id: 9) #user_category_id =14
+    Tag.create!(specialty: "Austin City Limits festival", category_id: 7)
     Tag.create!(specialty: "Pecan Street Festival", category_id: 7)
     Tag.create!(specialty: "UT Volleyball", category_id: 8)
     Tag.create!(specialty: "Kayaking on the Lake", category_id: 8)
@@ -231,27 +234,123 @@ Category.create!(title: "History", description: "Pay homage to the past", image:
     UserTag.create!(host_id: 8, tag_id: 39)
     UserTag.create!(host_id: 8, tag_id: 40)
 
-    #City 2 Seattle
-
-#City 3 Chicago
-  #Host 4
-    # UserCategory.create!(host_id: 7, category_id: 8)
-    # UserCategory.create!(host_id: 7, category_id: 3)
-    # Tag.create!(specialty: "Soccer clubs", category_id: 8) #tag_id = 22
-    # Tag.create!(specialty: "Chicago Cubs", category_id: 8) #tag_id = 23
-    # Tag.create!(specialty: "Cheap Eats", category_id: 3) #tag_id = 24
-    # Tag.create!(specialty: "Sports Bars", category_id: 3) #tag_id = 25
-    # Tag.create!(specialty: "Gastro-pubs", category_id: 3) #tag_id = 26
-    # UserTag.create!(host_id: 7, tag_id: 22)
-    # UserTag.create!(host_id: 7, tag_id: 23)
-    # UserTag.create!(host_id: 7, tag_id: 24)
-    # UserTag.create!(host_id: 7, tag_id: 25)
-    # UserTag.create!(host_id: 7, tag_id: 26)
+#City 2 Seattle
+  #Host 9
+    UserCategory.create!(host_id: 9, category_id: 1)
+    UserCategory.create!(host_id: 9, category_id: 2)
+    UserCategory.create!(host_id: 9, category_id: 3)
+    Tag.create!(specialty: "Seattle Art Museum", category_id: 1)
+    Tag.create!(specialty: "Bumbershoot Festival", category_id: 2)
+    Tag.create!(specialty: "Seafood", category_id: 3)
+    Tag.create!(specialty: "Japanese", category_id: 3)
+    UserTag.create!(host_id: 9, tag_id: 36)
+    UserTag.create!(host_id: 9, tag_id: 37)
+    UserTag.create!(host_id: 9, tag_id: 38)
+    UserTag.create!(host_id: 9, tag_id: 39)
+  #Host 10
+    UserCategory.create!(host_id: 10, category_id: 4)
+    UserCategory.create!(host_id: 10, category_id: 5)
+    UserCategory.create!(host_id: 10, category_id: 6)
+    Tag.create!(specialty: "Capitol Hill", category_id: 4)
+    Tag.create!(specialty: "Local bars", category_id: 4)
+    Tag.create!(specialty: "Local indigenous artisans", category_id: 5)
+    Tag.create!(specialty: "Mount Rainier", category_id: 6)
+    Tag.create!(specialty: "Hiking", category_id: 6)
+    UserTag.create!(host_id: 10, tag_id: 37)
+    UserTag.create!(host_id: 10, tag_id: 38)
+    UserTag.create!(host_id: 10, tag_id: 39)
+    UserTag.create!(host_id: 10, tag_id: 40)
+    UserTag.create!(host_id: 10, tag_id: 41)
+  #Host 11
+    UserCategory.create!(host_id: 11, category_id: 2) #user_category_id = 6
+    UserCategory.create!(host_id: 11, category_id: 3) #user_category_id = 7
+    UserCategory.create!(host_id: 11, category_id: 4) #user_category_id = 8
+    Tag.create!(specialty: "Punk Rock", category_id: 2) #tag_id = 9
+    Tag.create!(specialty: "Metal", category_id: 2) #tag_id = 10
+    Tag.create!(specialty: "Great American Cuisine", category_id: 3) #tag_id = 11
+    Tag.create!(specialty: "Highly Populated Bars", category_id: 4) #tag_id = 12
+    Tag.create!(specialty: "Chill Nightlife", category_id: 4) #tag_id = 13
+    UserTag.create!(host_id: 11, tag_id: 42)
+    UserTag.create!(host_id: 11, tag_id: 43)
+    UserTag.create!(host_id: 11, tag_id: 44)
+    UserTag.create!(host_id: 11, tag_id: 45)
+    UserTag.create!(host_id: 11, tag_id: 46)
+  #Host 12
+    UserCategory.create!(host_id: 12, category_id: 7) #user_category_id = 9
+    UserCategory.create!(host_id: 12, category_id: 8) #user_category_id = 9
+    UserCategory.create!(host_id: 12, category_id: 9) #user_category_id = 9
+    Tag.create!(specialty: "Sasquatch Festival", category_id: 7) #tag_id = 14
+    Tag.create!(specialty: "Seahawks football", category_id: 8) #tag_id = 15
+    Tag.create!(specialty: "Seattle underground", category_id: 9) #tag_id = 16
+    Tag.create!(specialty: "Historical earthquakes", category_id: 9) #tag_id = 17
+    Tag.create!(specialty: "Mt. Saint Helen's recap", category_id: 9) #tag_id = 18
+    UserTag.create!(host_id: 12, tag_id: 47)
+    UserTag.create!(host_id: 12, tag_id: 48)
+    UserTag.create!(host_id: 12, tag_id: 49)
+    UserTag.create!(host_id: 12, tag_id: 50)
+    UserTag.create!(host_id: 12, tag_id: 51)
+  #Host 13
+    UserCategory.create!(host_id: 13, category_id: 1) #user_category_id = 10
+    UserCategory.create!(host_id: 13, category_id: 2) #user_category_id = 11
+    UserCategory.create!(host_id: 13, category_id: 3) #user_category_id = 12
+    Tag.create!(specialty: "Asian-Pacific Art", category_id: 1) #tag_id = 19
+    Tag.create!(specialty: "Henry Art Gallery", category_id: 1) #tag_id = 20
+    Tag.create!(specialty: "All music at the Gorge", category_id: 2) #tag_id = 20
+    Tag.create!(specialty: "Chinese food", category_id: 3) #tag_id = 20
+    UserTag.create!(host_id: 13, tag_id: 52)
+    UserTag.create!(host_id: 13, tag_id: 53)
+    UserTag.create!(host_id: 13, tag_id: 54)
+    UserTag.create!(host_id: 13, tag_id: 55)
+  #Host 14
+    UserCategory.create!(host_id: 14, category_id: 4) #user_category_id = 13
+    UserCategory.create!(host_id: 14, category_id: 5) #user_category_id =14
+    UserCategory.create!(host_id: 14, category_id: 6) #user_category_id =14
+    Tag.create!(specialty: "Sports bars", category_id: 4) #tag_id = 21
+    Tag.create!(specialty: "Local vendors", category_id: 5) #tag_id = 21
+    Tag.create!(specialty: "Mountain biking", category_id: 6) #tag_id = 21
+    Tag.create!(specialty: "Snowmobiling", category_id: 6) #tag_id = 21
+    Tag.create!(specialty: "Skiing", category_id: 6) #tag_id = 21
+    UserTag.create!(host_id: 14, tag_id: 56)
+    UserTag.create!(host_id: 14, tag_id: 57)
+    UserTag.create!(host_id: 14, tag_id: 58)
+    UserTag.create!(host_id: 14, tag_id: 59)
+    UserTag.create!(host_id: 14, tag_id: 60)
+  #Host 15
+    UserCategory.create!(host_id: 15, category_id: 7) #user_category_id = 13
+    UserCategory.create!(host_id: 15, category_id: 8)
+    UserCategory.create!(host_id: 15, category_id: 9) #user_category_id =14
+    Tag.create!(specialty: "Capitol Hill Block Party", category_id: 7)
+    Tag.create!(specialty: "University of Washington games", category_id: 8)
+    Tag.create!(specialty: "Basketball", category_id: 8)
+    Tag.create!(specialty: "Sonics", category_id: 8)
+    Tag.create!(specialty: "Ferries", category_id: 9)
+    Tag.create!(specialty: "Pike's Place", category_id: 9) #tag_id = 21
+    UserTag.create!(host_id: 15, tag_id: 61)
+    UserTag.create!(host_id: 15, tag_id: 62)
+    UserTag.create!(host_id: 15, tag_id: 63)
+    UserTag.create!(host_id: 15, tag_id: 64)
+    UserTag.create!(host_id: 15, tag_id: 65)
+    UserTag.create!(host_id: 15, tag_id: 66)
 
 
 
   #Bookings ===============================================================================================
-    # Booking.create!(guest_id: 16, host_id: 1, meet_location: "Capitol building", summary: "Tour of the Capitol building, grounds, and a long walk down the infamous Congress Ave.")
+    Booking.create!(guest_id: 16, host_id: 1, summary: "A tour of the finest Art exhibits in Austin.", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "700 Congress Ave, Austin, TX 78701")
+    Booking.create!(guest_id: 16, host_id: 2, summary: "We are going to be enjoying the infamous 6th street.", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "715 W 6th St, Austin, TX 78701")
+    Booking.create!(guest_id: 16, host_id: 3, summary: "Only the punkest of rock gets into these venues.", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "305 E 5th Austin, TX 78701")
+    Booking.create!(guest_id: 17, host_id: 4, summary: "A tour of some of the hidden gems in Austin that only the most experienced Austinites know of.", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "3825 Lake Austin Blvd, Austin, TX 78703")
+    Booking.create!(guest_id: 17, host_id: 5, summary: "Let's party with some UT football!!!", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "405 E 23rd St, Austin, TX 78712")
+    Booking.create!(guest_id: 17, host_id: 6, summary: "The best Psychedelic Rock in Austin, hands down!", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "103 E 5th St B, Austin, TX 78701")
+    Booking.create!(guest_id: 18, host_id: 7, summary: "See all the best gay spots in Austin.", start_time: "2016-11-30 22:30:00 UTC", end_time: "2016-12-02 00:30:00 UTC", meet_location: "404 Colorado St, Austin, TX 78701" )
+    Booking.create!(guest_id: 18, host_id: 8, summary: "Tour of the Capitol building, grounds, and a long walk down the infamous Congress Ave.", meet_location: "1100 Congress Ave, Austin, TX 78701", start_time: "2016-11-30 18:30:00 UTC", end_time: "2016-12-01 20:30:00 UTC")
+
 
   #Reviews ===============================================================================================
-    # Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
+    Review.create!(booking_id: 1, body: "phacoidoscope demethylate unangry cosmogoner predisposed capability rocta daimen tisane improbative nonsecret Stockbridge unworshipping sparklingness trochilus ideogrammic Huchnom severingly thunderheaded ascribable Kulanapan inone fumous benitoite", rating: 3)
